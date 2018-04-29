af65f753abbc09bbec62f5199f0f5d07b745fdb9
James Childers <jamesalexchilders@gmail.com>
2018-04-29 13:01:21 -0500

    Fixed linting, added mocha tests. Verified tests run, linter runs, and dev-server runs and renders react component

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -1,11 +1,7 @@
-import { Component } from 'react';
+import React, { Component } from "react";
 
 export default class AppComponent extends Component {
-   render() {
-      return (
-         <div>
-            App Component is Rendered!!
-         </div>
-      )
-   }
+    render() {
+        return <div>App Component is Rendered!!</div>;
+    }
 }
---
+++ test/unit/test.spec.js
@@ -0,0 +1,5 @@
+describe("testing mocha", () => {
+    it("should just run this test", () => {
+        expect(0).to.eql(0);
+    });
+});
